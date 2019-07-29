76be2d37240217f283819f23be6b8eb90f3dfa72
Uberacy Tottoli <bera@boldtech.com.br>
2019-07-29 08:17:38 -0300

    Remove class

--- RabbirMQExample.Test/StandardQueueTests.cs
+++ RabbirMQExample.Test/StandardQueueTests.cs
@@ -10,13 +10,13 @@ namespace RabbitMQExample.Test
         [Fact]
         public void Create_Standard_Queue()
         {
+            string QueueName = "StandardQueue_ExampleQueue";
             var payment1 = new Payment { AmountToPay = 10, CardNumber = "2948484", Name = "Bera" };
             var payment2 = new Payment { AmountToPay = 20, CardNumber = "8848474", Name = "João" };
             var _sq = new StandardQueueExample(QueueName);
-            _sq.CreateStandardQueue();
+            //_sq.CreateStandardQueue();
             _sq.SendMessage(payment1);
             _sq.SendMessage(payment2);
-
             var recieve = _sq.Recieve();
         }
     }
--- RabbitMQExample/Program.cs
+++ RabbitMQExample/Program.cs
@@ -1,8 +1,4 @@
-﻿using RabbitMQ.Client;
-using RabbitMQExample.AMQP.Standard_Queue;
-using RabbitMQExample.Model;
-using System;
-
+﻿
 namespace RabbitMQExample
 {
     class Program
@@ -10,21 +6,6 @@ namespace RabbitMQExample
 
         static void Main(string[] args)
         {
-            CreateExchange();
-        }
-
-        public static void CreateExchange()
-        {
-            string QueueName = "StandardQueue_ExampleQueue";
-
-            var payment1 = new Payment { AmountToPay = 10, CardNumber = "2948484", Name = "Bera" };
-            var payment2 = new Payment { AmountToPay = 20, CardNumber = "8848474", Name = "João" };
-            var _sq = new StandardQueueExample(QueueName);
-            //_sq.CreateStandardQueue();
-            _sq.SendMessage(payment1);
-            _sq.SendMessage(payment2);
-
-            var recieve = _sq.Recieve();
         }
     }
 }
