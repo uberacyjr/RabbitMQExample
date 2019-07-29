1e6a9b50a3f518987be952aad67d3c0222283d6a
Uberacy Tottoli <bera@boldtech.com.br>
2019-07-29 08:15:47 -0300

    Change test project from .Net Standard Class Lib to .Net Class Lib

---
+++ RabbirMQExample.Test/StandardQueueTests.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQExample.AMQP.Standard_Queue;
+using RabbitMQExample.Model;
+using Xunit;
+
+namespace RabbitMQExample.Test
+{
+    public class StandardQueueTests
+    {
+        private string QueueName = "StandardQueue_ExampleQueue";
+        [Fact]
+        public void Create_Standart_Queue()
+        {
+            var payment1 = new Payment { AmountToPay = 10, CardNumber = "2948484", Name = "Bera" };
+            var payment2 = new Payment { AmountToPay = 20, CardNumber = "8848474", Name = "João" };
+            var _sq = new StandardQueueExample(QueueName);
+            _sq.CreateStandardQueue();
+            _sq.SendMessage(payment1);
+            _sq.SendMessage(payment2);
+
+            var recieve = _sq.Recieve();
+        }
+    }
+}
